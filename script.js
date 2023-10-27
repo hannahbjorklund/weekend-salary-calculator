6cfe0bebe745fe3b7ed4e54b10515b19ad68d08b
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-27 15:43:40 -0500

    Added code that makes a new table row containing user submitted information, and then clears the form fields after submitting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Oh hi there");
+console.log("Oh hi there, JS is working!");
 
 // Take in form submission data and add a new row to table. 
 function handleSubmit(event){
@@ -11,8 +11,23 @@ function handleSubmit(event){
     let title = document.getElementById("title").value;
     let annualSalary = document.getElementById("annualSalary").value;
     // Check variables
-    console.log(`${firstName}, ${lastName}, ${idNum}, ${title}, ${annualSalary}`);
+    console.log(`User submitted: ${firstName}, ${lastName}, ${idNum}, ${title}, ${annualSalary}`);
     // Create a variable for the table body, where we would like to add new information in a row
     let tableBody = document.getElementById("tableBody");
-    
+    // Add a new table row with all the form data
+    tableBody.innerHTML += 
+    `<tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNum}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>
+        <td><button>Delete</button></td>
+    </tr>`;
+    // Clear the form fields
+    document.getElementById("firstName").value = '';
+    document.getElementById("lastName").value = '';
+    document.getElementById("idNum").value = '';
+    document.getElementById("title").value = '';
+    document.getElementById("annualSalary").value = '';
 }
