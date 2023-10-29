a5f8140a2bbd2e4835085c221c47c954a2c248b9
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-29 00:32:44 -0500

    Made deleteEmployee remove red color from footer if under budget. Did some code formatting

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ let totalMonthly = 0;
 function handleSubmit(event){
     // Prevent form submission from refreshing the page
     event.preventDefault();
+    
     // Collect form data and store in variables
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
     let idNum = document.getElementById("idNum").value;
     let title = document.getElementById("title").value;
     let annualSalary = document.getElementById("annualSalary").value;
-    // Check variables
     console.log(`User submitted: ${firstName}, ${lastName}, ${idNum}, ${title}, ${annualSalary}`);
+    
     // Create a variable for the table body, where we would like to add new information in a row
     let tableBody = document.getElementById("tableBody");
+    
     // Add a new table row with all the form data
     tableBody.innerHTML += 
     `<tr>
@@ -26,6 +28,7 @@ function handleSubmit(event){
         <td>$${annualSalary}</td>
         <td><button onclick='deleteEmployee(event,${annualSalary})'>Delete</button></td>
     </tr>`;
+
     // Clear the form fields
     document.getElementById("firstName").value = '';
     document.getElementById("lastName").value = '';
@@ -33,12 +36,14 @@ function handleSubmit(event){
     document.getElementById("title").value = '';
     document.getElementById("annualSalary").value = '';
 
-    // Update total monthly
+    // Update total monthly expense
     totalMonthly += annualSalary/12;
     console.log("Total monthly:", totalMonthly);
+    
     // Create a variable for the span, where we will update the total monthly cost
     let span = document.getElementById("total-monthly");
     let footer = document.getElementById("footer")
+    
     // Make total monthly red if the total exceeds $20,000, else leave it black
     span.innerHTML = `$${totalMonthly.toFixed(2)}`;
     if(totalMonthly > 20000){
@@ -50,12 +55,19 @@ function handleSubmit(event){
 function deleteEmployee(event, annualSalary){
     console.log("You wish to delete this employee.");
     console.log("Annual salary of removed employee:", annualSalary, typeof annualSalary);
+    
     //Must use parentElement twice since we want to delete the entire row, not just the button itself
     event.target.parentElement.parentElement.remove();
+    
+    // Update the total monthly expense to reflect the deletion
     totalMonthly -= annualSalary/12;
     console.log("Total monthly:", totalMonthly);
     let span = document.getElementById("total-monthly");
+    let footer = document.getElementById("footer");
     span.innerHTML = `$${totalMonthly.toFixed(2)}`;
-    // Remove the over-budget class if the updated total monthly is less than 20,000
-    footer.classList.remove('over-budget');
+    
+    // Remove red color if the deletion puts the expense below budget
+    if(totalMonthly <= 20000){
+        footer.classList.remove('over-budget');
+    }
 }
